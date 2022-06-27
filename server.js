833387331811694f29d538b685b9ed7e49ed84b1
CameronTwiggs <cameron.020617@gmail.com>
2022-06-27 19:26:17 -0400

    bruh

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 //create express server
 const express = require('express');
 const mysql = require('mysql');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 8080;
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
 const connection = mysql.createConnection({
