9bd93e66de10a529c96602f320b189f9f9824081
CameronTwiggs <cameron.020617@gmail.com>
2022-06-27 17:43:38 -0400

    add

--- server2.js
+++ server2.js
@@ -3,6 +3,8 @@ const express = require('express');
 let app = express();
 let port = 8080;
 
+app.listen(port, () => console.log(`Listening on port ${port}`)); // listen on port 8080
+
 app.get('/', (req, res) => {
     res.send('bruh');
 } // send response to client
