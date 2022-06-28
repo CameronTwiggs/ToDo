33093c040a7b39f9263f43210d44319ccb58d8d4
CameronTwiggs <cameron.020617@gmail.com>
2022-06-27 20:34:52 -0400

    attempt 4

--- server2.js
+++ server2.js
@@ -1,13 +1,29 @@
-// starts expres server
 const express = require('express');
+const https = require('https');
+const fs = require('fs');
 let app = express();
 let port = 8080;
 
-app.listen(port, () => console.log(`Listening on port ${port}`)); // listen on port 8080
 
-app.get('/', (req, res) => {
-    res.send('bruh');
+var key = fs.readFileSync(__dirname + '/certs/selfsigned.key');
+var cert = fs.readFileSync(__dirname + '/certs/selfsigned.crt');
+var options = {
+  key: key,
+  cert: cert
+};
+
+
+
+app.get('/bruh', (req, res) => {
+    res.send('bruh but https');
 } // send response to client
 );
+
+
+var server = https.createServer(options, app);
+server.listen(port, () => {
+    console.log("server starting on port : " + port)
+  });
+
 
 
