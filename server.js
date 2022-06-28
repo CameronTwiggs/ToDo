520f5739c4e2a323dd747a069201496d3ab693c3
CameronTwiggs <cameron.020617@gmail.com>
2022-06-27 21:12:08 -0400

    same as last

---
+++ server.js
@@ -0,0 +1,12 @@
+// creatrs express server that sends folder contents to client
+const express = require('express');
+const app = express();
+const path = require('path');
+const fs = require('fs');
+const port = 8080;
+
+add.listen(port, () => {
+    console.log(`listening on port ${port}`);
+});
+
+
