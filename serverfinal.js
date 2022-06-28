39d0d1347c282ad60fa8241c8d5318224054484f
CameronTwiggs <cameron.020617@gmail.com>
2022-06-27 20:57:24 -0400

    im lost

---
+++ serverfinal.js
@@ -0,0 +1,36 @@
+//create express server
+const express = require('express');
+const mysql = require('mysql');
+const app = express();
+const port = 8080;
+
+
+const connection = mysql.createConnection({
+    host: 'todo-database.cxjiy2ijjdts.us-east-1.rds.amazonaws.com',
+    user: 'root',
+    port:  3306,
+    password: '[Twigs2112]',
+    database: 'todoDatabase'
+});
+
+connection.connect(function(err) {  // 
+    if(err) throw err;
+    console.log('Connected to database');
+}); // connect to database
+
+
+app.listen(port, () => console.log(`Listening on port ${port}`)); // listen on port 8080
+
+app.get ('/', (req, res) => {
+    const sql = 'SELECT * FROM tasks';
+    connection.query(sql, function(err, result) {
+        if(err) throw err;
+        res.send(result);
+    });
+}); // send response to client
+
+
+app.get ('/bruh', (req, res) => {
+    res.send('bruh');
+}); // send response to client
+
